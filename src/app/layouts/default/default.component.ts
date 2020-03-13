15741c1166467cb4fe281c2055f699437a29b320
Luiz Romario Filho <luizromariofilho@gmail.com>
2020-03-13 13:32:17 -0300

    Adding sidebar and header menu

--- src/app/layouts/default/default.component.ts
+++ src/app/layouts/default/default.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DefaultComponent implements OnInit {
 
+  sideBarOpen = true;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  sideBarToogler(){
+    this.sideBarOpen = !this.sideBarOpen;
   }
 
 }
--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -3,17 +3,25 @@ import { CommonModule } from '@angular/common';
 import { DefaultComponent } from './default.component';
 import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
+import { PostsComponent } from 'src/app/modules/posts/posts.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatDividerModule } from '@angular/material/divider';
 
 
 
 @NgModule({
   declarations: [
     DefaultComponent,
-    DashboardComponent
+    DashboardComponent,
+    PostsComponent
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    SharedModule,
+    MatSidenavModule,
+    MatDividerModule
   ]
 })
 export class DefaultModule { }
